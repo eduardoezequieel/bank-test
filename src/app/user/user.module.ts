6458537dde516a7980d4e6a334f81c84646b6e89
Eduardo Ezequiel <riv.edu10@gmail.com>
2023-05-24 18:31:46 -0600

    Add initial config

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {UserComponent} from './user.component';
+import {UserRoutingModule} from "./user-routing.module";
+import { FormLayoutComponent } from './layout/form-layout/form-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserComponent,
+    FormLayoutComponent
+  ],
   imports: [
+    UserRoutingModule,
     CommonModule
   ]
 })
-export class UserModule { }
+export class UserModule {
+}
