4f5f35f0b651f4bce371d2cdd72117ff1f64c0db
Eduardo Ezequiel <riv.edu10@gmail.com>
2023-05-24 23:14:58 -0600

    Add form styles

---
+++ src/app/user/components/beneficiaries-form/beneficiaries-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beneficiaries-form',
+  templateUrl: './beneficiaries-form.component.html',
+  styleUrls: ['./beneficiaries-form.component.scss']
+})
+export class BeneficiariesFormComponent {
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,19 +1,14 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {UserComponent} from './user.component';
-import {UserRoutingModule} from "./user-routing.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './user.component';
+import { UserRoutingModule } from './user-routing.module';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 import { FormLayoutComponent } from './layout/form-layout/form-layout.component';
-
+import { BeneficiariesFormComponent } from './components/beneficiaries-form/beneficiaries-form.component';
 
 @NgModule({
-  declarations: [
-    UserComponent,
-    FormLayoutComponent
-  ],
-  imports: [
-    UserRoutingModule,
-    CommonModule
-  ]
+  declarations: [UserComponent, FormLayoutComponent, BeneficiariesFormComponent],
+  imports: [UserRoutingModule, NgxMaskDirective, NgxMaskPipe, CommonModule],
+  providers: [provideNgxMask()],
 })
-export class UserModule {
-}
+export class UserModule {}
